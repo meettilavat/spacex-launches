3ecf318bf05336874d75dd6d93509d5f4adc8d1f
meettilavat <38155303+meettilavat@users.noreply.github.com>
2023-05-15 19:48:42 +0530

    init

--- src/Filter.js
+++ src/Filter.js
@@ -21,6 +21,15 @@ const Filter = ({ onFilterChange, years, filters }) => {
         <div className={styles.filter}>
             <h2>Filters</h2>
             <div>
+                <h3>Launch Year:</h3>
+                <div className={styles.yearContainer}>
+                    <button className={filters.launch_year === null ? styles.selected : ''} onClick={() => handleChange("launch_year", "all")}>All</button>
+                    {years.map(year => (
+                        <button key={year} className={filters.launch_year === year.toString() ? styles.selected : ''} onClick={() => handleChange("launch_year", year.toString())}>{year}</button>
+                    ))}
+                </div>
+            </div>
+            <div>
                 <h3>Launch Success:</h3>
                 <button className={filters.launch_success === "true" ? styles.selected : ''} onClick={() => handleChange("launch_success", "true")}>True</button>
                 <button className={filters.launch_success === "false" ? styles.selected : ''} onClick={() => handleChange("launch_success", "false")}>False</button>
@@ -29,15 +38,6 @@ const Filter = ({ onFilterChange, years, filters }) => {
                 <h3>Land Success:</h3>
                 <button className={filters.land_success === "true" ? styles.selected : ''} onClick={() => handleChange("land_success","true")}>True</button>
                 <button className={filters.land_success === "false" ? styles.selected : ''} onClick={() => handleChange("land_success", "false")}>False</button>
-            </div>
-            <div>
-                <h3>Launch Year:</h3>
-                <div className={styles.yearContainer}>
-                    <button className={filters.launch_year === null ? styles.selected : ''} onClick={() => handleChange("launch_year", "all")}>All</button>
-                    {years.map(year => (
-                        <button key={year} className={filters.launch_year === year.toString() ? styles.selected : ''} onClick={() => handleChange("launch_year", year.toString())}>{year}</button>
-                    ))}
-                </div>
             </div>
         </div>
     );
